77fbe0b9f83786f083b9d0e96e03a5cd859b86a0
NahlApps <165295576+NahlApps@users.noreply.github.com>
2025-09-19 13:50:33 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,3 +0,0 @@
-// config.js — set your Apps Script deployment URL
-// Example: https://script.google.com/macros/s/AKfycbx.../exec
-window.NAHL_API_BASE = "https://script.google.com/macros/s/AKfycbyYcWNPa2zeMXTWMEcQi5pkGN_82JGDGy2MPvxa9gVc5RqGlRA6-S-2CcqLBGaErtki/exec";
