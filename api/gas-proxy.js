179c76104f0cb5c8322a24c36c957d588dfdb829
NahlApps <165295576+NahlApps@users.noreply.github.com>
2025-10-07 16:42:13 +0300

    Update gas-proxy.js

--- api/gas-proxy.js
+++ api/gas-proxy.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   }
 
   // Allow your site & preflight
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.spongnsoap.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://demotesting.nahl.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') {
